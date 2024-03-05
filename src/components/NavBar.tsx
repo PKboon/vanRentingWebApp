89d15a78b2f0d8d1acbfc7e9f99cbf650f4d22d5
Pikulkaew Boonpeng <pkboonpeng@gmail.com>
2024-03-04 20:45:50 -0500

    delete unused lines

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,6 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
-interface Props extends React.ComponentProps<'header'> {
-
-}
-
-const NavBar = ({ ...props }: Props) => {
+const NavBar = () => {
     return (
         <header className="d-flex justify-content-between align-items-center py-2 px-4 bg-light">
             <Link to='/' className="text-decoration-none">
